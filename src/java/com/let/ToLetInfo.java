035d7e3d083fb6dc45d0b7123f19186b980d0bdd
netiworld <absiddik.netizen@gmail.com>
2018-02-04 23:07:30 +0600

    new jsp tolet

---
+++ src/java/com/let/ToLetInfo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.let;
+
+/**
+ *
+ * @author Dinislam
+ */
+public class ToLetInfo {
+    public static void main(String[] args) {
+        
+    }
+}
