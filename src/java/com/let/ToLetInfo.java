06da6d26ec488997ab6e2b9a01821897de5e22f8
netiworld <absiddik.netizen@gmail.com>
2018-02-04 23:55:05 +0600

    jsptolet file all are commited and toletinfo java file forloop is done by din

--- src/java/com/let/ToLetInfo.java
+++ src/java/com/let/ToLetInfo.java
@@ -10,7 +10,21 @@ package com.let;
  * @author Dinislam
  */
 public class ToLetInfo {
+
     public static void main(String[] args) {
-        
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int x : numbers) {
+            System.out.print(x);
+            System.out.print(",");
+        }
+        System.out.print("\n");
+        String[] names = {"James", "Larry", "Tom", "Lacy"};
+
+        for (String name : names) {
+            System.out.print(name);
+            System.out.print(",");
+        }
     }
+
 }
